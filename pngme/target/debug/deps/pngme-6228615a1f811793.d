4ed8cc45ef32dbdec29ad099402e4b45cb2ae8f4
Doku-Desktop <joonkim2684@gmail.com>
2023-12-30 16:59:28 +0900

    initialize project

---
+++ pngme/target/debug/deps/pngme-6228615a1f811793.d
@@ -0,0 +1,5 @@
+/Users/joonkim/coding/Rust/PNGme/pngme/target/debug/deps/libpngme-6228615a1f811793.rmeta: src/main.rs
+
+/Users/joonkim/coding/Rust/PNGme/pngme/target/debug/deps/pngme-6228615a1f811793.d: src/main.rs
+
+src/main.rs:
---
+++ pngme/target/debug/deps/pngme-90f73ac114889ab1.d
@@ -0,0 +1,5 @@
+/Users/joonkim/coding/Rust/PNGme/pngme/target/debug/deps/libpngme-90f73ac114889ab1.rmeta: src/main.rs
+
+/Users/joonkim/coding/Rust/PNGme/pngme/target/debug/deps/pngme-90f73ac114889ab1.d: src/main.rs
+
+src/main.rs:
